800226762e20b06c64d9e97c63616071effa6420
mhbach22021224 <124248245+mhbach22021224@users.noreply.github.com>
2023-11-10 21:28:15 +0700

    Add files via upload

--- Dictionary.java
+++ Dictionary.java
@@ -1,15 +1,74 @@
-package Base;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+package org.example;
+
+import java.sql.SQLOutput;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
+import java.util.Scanner;
 
-public class Dictionary {
-    public static ArrayList<Word> words = new ArrayList<>();
+class Dictionary {
+    public static ArrayList<Word> words;
 
+    // constructor
+    public Dictionary() {
+        this.words = new ArrayList<>();
+    }
+
+    // thêm từ vào từ điển
+    public void addWord(Word word) {
+        words.add(word);
+    }
+
+
+
+    // hiển thị tất cả từ trong từ điển
+    public void showAllWords() {
+        System.out.println("No | English | Vietnamese");
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println((i + 1) + " | " + words.get(i).getWord_target() + " | " + words.get(i).getWord_explain());
+        }
+    }
+    //get words
     public ArrayList<Word> getWords() {
         return words;
+    }
+    public String getVie(String t) {
+        t = t.trim();
+        for(int i=0; i < words.size() ; i++) {
+            String newstr = words.get(i).getWord_target().trim();
+            if( newstr.compareTo(t) == 0) {
+                return words.get(i).getWord_explain();
+            }
+        }
+        return null;
+    }
+    public void removeVie(String word) {
+        word = word.trim();
+        for(int i=0; i < words.size() ; i++) {
+            String newstr = words.get(i).getWord_target().trim();
+            if( newstr.compareTo(word) == 0) {
+                words.remove(i);
+            }
+        }
+    }
+    public void addVie() {
+        System.out.println("Nhap Tu tieng Viet");
+        Scanner sc = new Scanner(System.in);
+        String Vie = sc.nextLine();
+        System.out.println("Nhap Tu tieng Anh");
+        String Eng = sc.nextLine();
+        words.add(new Word(Vie,Eng));
+        System.out.println("success");
+    }
+    public void setVie(String word) {
+        System.out.println("Nhap nghia muon thay doi");
+        Scanner sc = new Scanner(System.in);
+        String newword = sc.nextLine();
+        newword = newword.trim();
+        for(int i=0; i < words.size() ; i++) {
+            String newstr = words.get(i).getWord_target().trim();
+            if( newstr.compareTo(word) == 0) {
+                words.get(i).setWord_target(newword);
+            }
+        }
     }
 }
--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,90 +1,36 @@
-package Base;
-import javafx.scene.Scene;
-
-import java.util.*;
+package org.example;
 import java.io.*;
-import java.util.Dictionary;
-
-public class DictionaryManagement extends Base.Dictionary {
-    public static void insertFromFile(Dictionary dictionary, String path) {
-         try {
-             File file = new File("C:/Users/MAI HOANG BACH/IdeaProjects/project/src/main/resources/Dictionaries.txt");
-             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-             String line = bufferedReader.readLine();
-             line = line.replace("@", "");
-             System.out.println(line);
-         } catch (FileNotFoundException e) {
-             throw new RuntimeException(e);
-         } catch (IOException e) {
-             throw new RuntimeException(e);
-         }
-    }
-
-    public static int isContain(String str1, String str2) {
-        for (int i = 0; i < Math.min(str1.length(), str2.length()); i++) {
-            if (str1.charAt(i) > str2.charAt(i)) {
-                return 1;
-            } else if (str1.charAt(i) < str2.charAt(i)) {
-                return -1;
+import java.util.Scanner;
+class DictionaryManagement {
+    // nhập liệu từ bàn phím và thêm vào từ điển
+    public void inserFromFile(Dictionary dictionary) throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+        FileReader fr = new FileReader("datatest.txt");
+        BufferedReader br = new BufferedReader(fr);
+        while (true){
+            String word_target = "";
+            String word_explain = "";
+            boolean checkE = true;
+            String s = br.readLine();
+            if(s == null) {
+                break;
             }
-        }
-        if (str1.length() > str2.length()) {
-            return 1;
-        }
-        return 0;
-    }
-    public static int binarySearch(int left, int right, String word) {
-        if (right < left) {
-            return -1;
-        }
-        int mid = left + (right - left) / 2;
-        int compare = isContain(word, words.get(mid).getWord_target());
-        if (compare == -1) {
-            return binarySearch(left, mid - 1, word);
-        } else if (compare == 1) {
-            return binarySearch(mid + 1, right, word);
-        } else {
-            return mid;
-        }
-    }
-
-    public static void dictionaryLookup() {
-         Scanner sc = new Scanner(System.in);
-         String word = sc.next();
-         int index = binarySearch(0, words.size(), word);
-    }
-    public static void addWord(Word word, String path) {
-        try {
-            FileWriter fileWriter = new FileWriter(path, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write("@" + word.getWord_target() + '\n' + "*" + word.getWord_type()
-                    + '\n' + "-" + word.getWord_explain() + '\n' + "=" + word.getWord_example());
-            bufferedWriter.newLine();
-        } catch(FileNotFoundException e) {
-            System.out.println("File error!");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void updateWord() {
-
-    }
-
-    public static void removeWord() {
-
-    }
-
-    public static void main(String[] args) {
-        try {
-            File file = new File("C:/Users/MAI HOANG BACH/IdeaProjects/project/src/main/resources/Dictionaries.txt");
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            String line;
-            while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+            for(int i = 0; i < s.length() ; i++) {
+                if( s.charAt(i) == ':' ) {
+                    checkE = false;
+                    continue;
+                }
+                if(checkE == true) {
+                    word_target+=s.charAt(i);
+                }
+                else {
+                    word_explain += s.charAt(i);
+                }
             }
-        } catch (Exception e) {
-
+            word_target = word_target.trim();
+            word_explain = word_explain.trim();
+            Word word = new Word(word_target, word_explain);
+            dictionary.addWord(word);
         }
     }
 }
