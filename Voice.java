eb5918b0605f5c8e547709b0b968b8e812058e95
mhbach22021224 <124248245+mhbach22021224@users.noreply.github.com>
2023-11-13 21:53:10 +0700

    Add files via upload

---
+++ Voice.java
@@ -0,0 +1,37 @@
+package base;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import javazoom.jl.player.Player;
+
+public class Voice {
+
+    public static void speak(String speech) {
+        try {
+            String api =
+                    "https://translate.google.com/translate_tts?ie=UTF-8&tl="
+                            + "en"
+                            + "&client=tw-ob&q="
+                            + URLEncoder.encode(speech, StandardCharsets.UTF_8);
+            URL url = new URL(api);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            InputStream audio = con.getInputStream();
+            new Player(audio).play();
+            con.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error in getting voices");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String speech = sc.nextLine();
+        speak(speech);
+    }
+}
